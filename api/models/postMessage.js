9839f1e788b1bb957e57d0751854e93caf72f373
Abduh <mabduhsiregar@gmail.com>
2020-07-20 10:26:16 +0700

    first commit

---
+++ api/models/postMessage.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+var PostMessage = mongoose.model('PostMessage', {
+    title: {type:String},
+    message: {type:String},
+}, 'postMessages')
+
+module.exports = {PostMessage}
